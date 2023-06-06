5da3991a01642297374d9dc7297593d8d6f1577f
MariySemenenko <mashyni92@gmail.com>
2023-06-06 09:40:31 +0300

    query

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+
+export class App extends Component {
+
+  state = {
+   page: 1,
+   response: [],
+  };
+
+  render() {
+
+return (
+
+<>
+
+</>
+
+)
+
+  }
+}
+
+
+
+//https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12
+//user_id:35692363 
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+
+export class Searchbar extends Component {
+
+    state = {
+        images: [],
+        query: '',
+        page: 1,
+    };
+  
+    render() {
+  
+  return (
+  
+  <>
+  
+  </>
+  
+  )
+  
+    }
+  }
+  
