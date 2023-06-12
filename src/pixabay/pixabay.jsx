df658e1aa78d8089b4d03e7c8f9b029015c41b8d
MariySemenenko <mashyni92@gmail.com>
2023-06-12 11:08:50 +0300

    data

--- src/pixabay/pixabay.jsx
+++ src/pixabay/pixabay.jsx
@@ -1,25 +1,19 @@
 import axios from 'axios';
 
 export async function getImages(query, page) {
-  
   const BASE_URL = `https://pixabay.com/api/`;
 
   const API_KEY = '35692363-708937b69116888a5fb6ef1bd';
 
   const options = `image_type=photo&orientation=horizontal&per_page=12`;
-  
+
   try {
-    
     const { data } = await axios.get(
-      
       `${BASE_URL}?key=${API_KEY}&q=${query}&${options}&page=${page}`
-      
     );
+
     return data;
-    
   } catch (error) {
     console.log(error);
-    
   }
-  
 }
